537eeb4b8f6f9b07c7afb6117049673ceda58182
Monica Li <monicali@live.ca>
2020-11-15 22:16:20 +0000

    add functions to pass mocha tests

--- vampire.js
+++ vampire.js
@@ -10,22 +10,65 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numOfVamp = 0;
+    let currentVamp = this;
 
+    while (currentVamp.creator)  {
+      currentVamp = currentVamp.creator;
+      numOfVamp++;
+    }
+    return numOfVamp;
   }
 
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the
+  // original vampire)
   isMoreSeniorThan(vampire) {
+    let numOfCurrentVamp = 0;
+    let numOfInputVamp = 0;
+    let currentVamp = this;
 
+    while (currentVamp.creator)  {
+      currentVamp = currentVamp.creator;
+      numOfCurrentVamp++;
+    }
+    while (vampire.creator)  {
+      vampire = vampire.creator;
+      numOfInputVamp++;
+    }
+
+    if (numOfInputVamp > numOfCurrentVamp) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /** Tree traversal methods **/
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    
   }
 
   /** Stretch **/
